7e354fe1594299d411eeccbe7e4e4e35c873736d
Tman0202 <tomasmandefro@gmail.com>
2022-09-26 14:05:18 -0700

    lastcom3

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-""" a function that retrieves an element from a list like in C."""
+"""a function that prints all integers of a list."""
 
 
-def element_at(my_list, idx):
-    if idx < 0 or len(my_list) - 1 < idx:
-        return None
-    return my_list[idx]
+def print_list_integer(my_list=[]):
+    for integer in my_list:
+        print("{:d}".format(integer))
