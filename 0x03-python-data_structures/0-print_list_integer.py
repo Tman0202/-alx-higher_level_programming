be8008b1b9f32377177758122557a08c017bae4d
Tman0202 <tomasmandefro@gmail.com>
2022-09-26 13:33:31 -0700

    lastcom1

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""a function that prints all integers of a list."""
+
+
+def print_list_integer(my_list=[]):
+    for integer in my_list:
+        print("{:d}".format(integer))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+
