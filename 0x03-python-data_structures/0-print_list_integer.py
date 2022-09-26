2afaf4d7dc9dffdd6cb3ac6ea01ad1884c8047a1
Tman0202 <tomasmandefro@gmail.com>
2022-09-26 14:09:47 -0700

    lastcom4

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -6,9 +6,3 @@
 def print_list_integer(my_list=[]):
     for integer in my_list:
         print("{:d}".format(integer))
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-
--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-"""a function that prints all integers of a list."""
+""" a function that retrieves an element from a list like in C."""
 
 
-def print_list_integer(my_list=[]):
-    for integer in my_list:
-        print("{:d}".format(integer))
+def element_at(my_list, idx):
+    if idx < 0 or len(my_list) - 1 < idx:
+        return None
+    return my_list[idx]
