7fb0995ae03f3713ef9b4eaf2cb83c9b067b96b2
Tman0202 <tomasmandefro@gmail.com>
2023-01-17 13:36:15 -0800

    networkcom11

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -11,18 +11,6 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
 
-   # request = urllib.request.Request(url)
-   # with urllib.request.urlopen(request) as response:
-       # print(dict(response.headers).get("X-Request-Id"))
-
-       # with urllib.request.urlopen(sys.argv[1]) as response:
-#     headers = response.getheaders()
-#     for header in headers:
-#         if header[0] == "X-Request-Id":
-#             print(header[1])
-
-
-with urllib.request.urlopen(url) as response:
-    header = response.info()
-    request_id = header.get('X-Request-Id')
-    print(request_id)
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
