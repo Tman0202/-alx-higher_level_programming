38cc832561312d4a5fd99debe0afc767e22b8a21
Tman0202 <tomasmandefro@gmail.com>
2023-01-17 13:22:44 -0800

    networkcom1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""A script that
+- fetches the url: https://alx-intranet.hbtn.io/status.
+- uses urllib package
+"""
+
+
+if __name__ == '__main__':
+    
+    import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode('utf-8'))
